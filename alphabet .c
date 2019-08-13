1ab561d232de10bafabbd5f09aa9eab33639c47e
Vinni31 <54021486+Vinni31@users.noreply.github.com>
2019-08-13 23:32:46 +0530

    Create alphabet .c

---
+++ alphabet .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+{
+    char n;
+    scanf("%s",&n);
+    if ((n>='a'&&n<='z')||(n>='A'&&n<='Z'))
+    {
+        printf("alphabet");
+        
+    }
+        
+    else
+    {
+        printf("Invalid");
+        
+    }
+}
