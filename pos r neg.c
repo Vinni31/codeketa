e5cb1eecfe04ac322e8e4c0b10057fff3bcd87fc
Vinni31 <54021486+Vinni31@users.noreply.github.com>
2019-08-13 22:34:50 +0530

    Create pos r neg.c

---
+++ pos r neg.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>=1)
+    {
+        printf("positive");
+    }
+    else if(n==0)
+    {
+        printf("Zero");
+    }
+    else
+    {
+        printf("negative");
+    }
+}
+
+
